d0deafbe5fb94757b1005977bb4684ad43166095
amkkun <amkkun@gmail.com>
2013-06-23 20:42:19 +0900

    Add primary pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,20 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
-  
+
+  def info = Action {
+    Ok(views.html.info())
+  }
+
+  def gallery = Action {
+    Ok(views.html.gallery())
+  }
+
+  def howto = Action {
+    Ok(views.html.howto())
+  }
 }
