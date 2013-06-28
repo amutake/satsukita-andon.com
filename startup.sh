101dc32e69ec3cbe58b7f66c35ff1ea726b002cc
amkkun <amkkun@gmail.com>
2013-06-28 18:56:53 +0900

    Ignore some files

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ${0%/*}
-if [ -f RUNNING_PID ]; then
-    echo "already started"
-else
-    play clean compile stage && target/start -Dhttp.port=6039 -DapplyEvolutions.default=true &
-fi
