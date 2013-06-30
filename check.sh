22fb646a2037efd8c37e858b671dfe737b0a0897
amkkun <amkkun@gmail.com>
2013-06-30 12:15:28 +0900

    Add check script

---
+++ check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+result="OK"
+
+for file in $(find ./public/img/products -type f -name "*.jpg" -print | sed -e "s/products/thumbnails/g"); do
+    if [ ! -f $file ]; then
+        echo "Missing: $file"
+        result="Error"
+    fi
+done
+
+echo $result
