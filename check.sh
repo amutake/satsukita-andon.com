bcac166a0d102c6d83fcf10d14f28bbc253a4867
amkkun <amkkun@gmail.com>
2013-07-06 12:35:24 +0900

    Check

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 
 result="OK"
 
-for file in $(find ./public/img/products -type f -name "*.jpg" -print | sed -e "s/products/thumbnails/g"); do
+for file in $(find ./public/img/products -type f -print | sed -e "s/products/thumbnails/g"); do
     if [ ! -f $file ]; then
         echo "Missing: $file"
         result="Error"
