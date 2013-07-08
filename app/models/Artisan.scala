badb5cf261325c39592443ba0a7483191da3ae00
amkkun <amkkun@gmail.com>
2013-07-08 13:10:56 +0900

    Add artisan model

---
+++ app/models/Artisan.scala
@@ -0,0 +1,32 @@
+package models
+
+import play.api.db._
+import play.api.Play.current
+
+import scala.slick.driver.H2Driver.simple._
+
+import andon.utils._
+
+case class Artisan(id: Int, name: String, username: String, password: String, times: OrdInt)
+
+object Artisan extends Table[Artisan]("ARTISAN") {
+
+  val db = Database.forDataSource(DB.getDataSource("default"))
+
+  def id = column[Int]("ID", O.NotNull, O.PrimaryKey, O.AutoInc)
+  def name = column[String]("NAME", O.NotNull)
+  def username = column[String]("USERNAME", O.NotNull, O.PrimaryKey)
+  def password = column[String]("PASSWORD", O.NotNull)
+  def times = column[Int]("TIMES", O.NotNull)
+
+  def * = id ~ name ~ username ~ password ~ times <> (
+    (id, name, username, password, times) => Artisan(id, name, username, password, OrdInt(times)),
+    artisan => Some((artisan.id, artisan.name, artisan.username, artisan.password, artisan.times.n))
+  )
+
+  val query = Query(Artisan)
+
+  def findById(id: Int): Option[Artisan] = db.withSession { implicit session: Session =>
+    query.filter(_.id === id).firstOption
+  }
+}
