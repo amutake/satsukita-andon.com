aee73097ae30eabeb903cb101cb7538fe9ccc5f5
amkkun <amkkun@gmail.com>
2013-07-08 12:36:14 +0900

    Use slick for ClassData

--- app/models/ClassData.scala
+++ app/models/ClassData.scala
@@ -14,10 +14,16 @@ object ClassData extends Table[ClassData]("CLASSDATA") {
   val db = Database.forDataSource(DB.getDataSource("default"))
 
   def id = column[ClassId]("ID", O.NotNull, O.PrimaryKey)
+  def times = column[Int]("TIMES", O.NotNull)
+  def grade = column[Int]("GRADE", O.NotNull)
+  def classn = column[Int]("CLASSN", O.NotNull)
   def title = column[String]("TITLE", O.NotNull)
-  def prize = column[Option[Prize]]("PRIZE")
+  def prize = column[Option[String]]("PRIZE")
 
-  def * = id ~ title ~ prize <> (ClassData.apply _, ClassData.unapply _)
+  def * = id ~ times ~ grade ~ classn ~ title ~ prize <> (
+    (id, times, grade, classn, title, prize) => ClassData(id, title, prize.flatMap(Prize.fromString)),
+    data => Some((data.id, data.id.times.n, data.id.grade, data.id.classn, data.title, data.prize.map(_.toString)))
+  )
 
   val query = Query(ClassData)
 
@@ -30,20 +36,46 @@ object ClassData extends Table[ClassData]("CLASSDATA") {
   }
 
   def findByTimes(t: OrdInt) = db.withSession { implicit session: Session =>
-    // TODO
-    query.list.filter { data =>
-      data.id.times == t
-    }
+    query.filter(_.times === t.n).list
   }
 
   def search(times: String, prize: String, grade: String, tag: String) = db.withSession { implicit session: Session =>
 
-    val q = for {
-      t <- Tags if t.tag === tag
-      data <- ClassData if data.id === t.classId
-    } yield data
+    val q = if (times == "all") {
+      query
+    } else {
+      query.filter(_.times == times.toInt)
+    }
 
-    q.list
+    val q1 = if (prize == "all") {
+      q
+    } else if (prize == "none") {
+      q.filter(_.prize.isNull)
+    } else {
+      q.filter(_.prize === prize)
+    }
+
+    val q2 = if (grade == "all") {
+      q1
+    } else {
+      q1.filter(_.grade == grade.toInt)
+    }
+
+    val q3 = if (tag == "all") {
+      q2
+    } else {
+      val tagId = for {
+        t <- Tags if t.tag === tag
+      } yield t.classId
+
+      for {
+        id <- tagId
+        data <- q2
+        if data.id === id
+      } yield data
+    }
+
+    q3.list
   }
 
   def create(data: ClassData) = db.withSession { implicit session: Session =>
