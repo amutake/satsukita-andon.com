f71d004e5096e883efc7e67d630e7696293b81d3
amkkun <amkkun@gmail.com>
2013-07-11 16:54:33 +0900

    About and Contact

--- app/Global.scala
+++ app/Global.scala
@@ -98,8 +98,25 @@ object InitialData {
 
 記事の執筆者を募集しております。興味のある方は[Contactページ](http://satsukita-andon.com/contact)の連絡先までご連絡ください。
 """, now, now, InfoTop),
-          (1, 1, "About", "", now, now, About),
-          (1, 1, "Contact", "", now, now, Contact)
+          (1, 1, "About", """
+このサイトは札幌北高校の学校祭の行事である行灯行列の記録とその製作を支援するために、初代管理人の遊び人(51st)とその仲間たちによって作られたサイトです。
+
+二代目管理人 仕掛人(53rd)、三代目管理人 御家人(56th)、四代目管理人 案内人(58th)と続き、現在は五代目管理人の甲乙人(60th)がサイトの運営をしております。
+
+[2010年以前のサイト](http://old.satsukita-andon.com)
+
+
+# 掲載画像について
+
+掲載されている画像には、個人の顔が写っている写真もあります。不都合がある場合にはお知らせください。即刻削除致します。
+""", now, now, About),
+          (1, 1, "Contact", """
+このサイトに関する意見、要望、バグなど、また、甲乙人個人へのお問い合わせは下記の連絡先までご連絡ください。
+
+- メールアドレス: andon.kohotsunin [at] gmail.com
+- Twitter: [甲乙人](https://twitter.com/kohotsunin)
+- GitHub: [行灯職人への道のGitHubリポジトリ](https://github.com/amkkun/satsukita-andon.com)へのPull RequestまたはIssue登録
+""", now, now, Contact)
         )
       }
     }
