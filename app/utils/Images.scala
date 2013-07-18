c495f695346514c0a351bd87aa5915898aa3bc17
amkkun <amkkun@gmail.com>
2013-07-18 13:18:40 +0900

    Move gallery image to files/gallery

--- app/utils/Images.scala
+++ app/utils/Images.scala
@@ -7,24 +7,23 @@ import models._
 object Images {
 
   def productsDir(t: OrdInt, g: Int, c: Int) =
-    "./public/img/products/" + t + "/" + g + "/" + c
+    "./files/gallery/fullsize/" + t + "/" + g + "/" + c
 
   def thumbnailsDir(t: OrdInt, g: Int, c: Int) =
-    "./public/img/thumbnails/" + t + "/" + g + "/" + c
+    "./files/gallery/thumbnail/" + t + "/" + g + "/" + c
 
   def img(t: OrdInt, g: Int, c: Int, n: Int, f: (OrdInt, Int, Int) => String) =
     f(t, g, c) + "/" + t + g + "-" + c + "_" + pictNumber(n) + ".jpg"
 
   def pictNumber(n: Int) = if (n < 10) ("0" + n) else (n.toString)
 
-  def toThumbnail(path: String) =
-    "img/thumbnails" + path.substring("img/products".length)
+  def toThumbnail(path: String) = """/fullsize/""".r.replaceFirstIn(path, "/thumbnail/")
 
   def getClassImages(t: OrdInt, g: Int, c: Int): Seq[String] = {
     val path = productsDir(t, g, c)
     val dir = new File(path)
     if (dir.isDirectory) {
-      dir.listFiles.map(_.getPath.substring("./public/".length))
+      dir.listFiles.map(_.getPath.substring(1))
     } else {
       Seq()
     }
@@ -47,7 +46,7 @@ object Images {
       val path = img(c.id.times, c.id.grade, c.id.classn, 1, thumbnailsDir)
       val file = new File(path)
       if (file.exists) {
-        (c, file.getPath.substring("./public/".length))
+        (c, file.getPath.substring(1))
       } else {
         (c, "img/logo.png")
       }
