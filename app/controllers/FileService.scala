7ffa974571ac63b59f367a0209358fbf312add34
amkkun <amkkun@gmail.com>
2013-07-25 16:32:51 +0900

    Supress a warning

--- app/controllers/FileService.scala
+++ app/controllers/FileService.scala
@@ -10,7 +10,7 @@ object FileService extends Controller {
   val dir = "./files/"
 
   def get(filename: String) = Action {
-    val decoded = java.net.URLDecoder.decode(filename)
+    val decoded = java.net.URLDecoder.decode(filename, "UTF-8")
     val file = new File(dir + decoded)
     if (file.exists()) {
       Ok.sendFile(file)
