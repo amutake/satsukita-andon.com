cdaecf07ae183e51417478f4ed56017597c84d3e
amkkun <amkkun@gmail.com>
2013-07-25 16:23:43 +0900

    Fix #34: decode url

--- app/controllers/FileService.scala
+++ app/controllers/FileService.scala
@@ -10,11 +10,12 @@ object FileService extends Controller {
   val dir = "./files/"
 
   def get(filename: String) = Action {
-    val file = new File(dir + filename)
+    val decoded = java.net.URLDecoder.decode(filename)
+    val file = new File(dir + decoded)
     if (file.exists()) {
       Ok.sendFile(file)
     } else {
-      NotFound(views.html.errors.notFound("/files/" + filename))
+      NotFound(views.html.errors.notFound("/files/" + decoded))
     }
   }
 }
