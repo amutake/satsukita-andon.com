5729e1e1e2878a90fe1840d31c4a05ee3af163b4
amutakes <amutakes@gmail.com>
2013-10-19 10:07:50 +0900

    Add robots.txt

--- app/controllers/FileService.scala
+++ app/controllers/FileService.scala
@@ -7,15 +7,15 @@ import java.io.File
 
 object FileService extends Controller {
 
-  val dir = "./files/"
+  def get(filename: String) = getByPath("/files/" + filename)
 
-  def get(filename: String) = Action {
-    val decoded = java.net.URLDecoder.decode(filename, "UTF-8")
-    val file = new File(dir + decoded)
+  def getByPath(path: String) = Action {
+    val decoded = java.net.URLDecoder.decode(path, "UTF-8")
+    val file = new File("." + decoded)
     if (file.exists()) {
       Ok.sendFile(file)
     } else {
-      NotFound(views.html.errors.notFound("/files/" + decoded))
+      NotFound(views.html.errors.notFound(decoded))
     }
   }
 }
