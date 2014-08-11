6f6b063f26e8fa74ca4fd08a9de442d2510d6f7d
amutake <amutake.s@gmail.com>
2014-08-11 20:55:19 +0900

    Update andon command

--- db/backup.sh
+++ db/backup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -cp h2.jar org.h2.tools.Script -url jdbc:h2:default -user "" -password "" -script backup.sql
+cp default.h2.db backup.h2.db
+java -cp h2.jar org.h2.tools.Script -url jdbc:h2:backup -user "" -password "" -script backup.sql
+rm backup.h2.db
