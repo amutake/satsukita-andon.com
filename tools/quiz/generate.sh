d18e092a8886a9bf84ab465cf262207203315849
amutake <amutake.s@gmail.com>
2015-07-27 16:12:50 +0900

    update quiz

--- tools/quiz/generate.sh
+++ tools/quiz/generate.sh
@@ -5,7 +5,7 @@ NAME="quiz"
 
 cd ${0%/*}
 
-zip -r ${NAME}.zip * -x generate.sh *.zip
+zip -r ${NAME}.zip * -x generate.sh *.zip *.md
 curl -o generated.zip -F "file=@./${NAME}.zip" http://quizgenerator.net/quizgen${VERSION}/index.php?action=generate
 rm ${NAME}.zip
 rm -r ../../public/quiz/*
